584c4f58024ba59806f081d2e6afdcc6e4a62f87
CiYuan_awa <iq-boost@awa.gold>
2024-08-25 02:43:34 +0800

    细节优化

--- MonitorBot/Program.cs
+++ MonitorBot/Program.cs
@@ -4,10 +4,10 @@ namespace MonitorBot
 {
     public class Program
     {
-        public const string Version = "1.0.2";
+        public const string Version = "1.0.1";
         public static Task Main(string[] args)
         {
-            Console.WriteLine($"CiYuan Util Kook Bot Version {Version}");
+            Console.WriteLine($"Monitor Kook Bot Version {Version}");
             try { using StreamReader sr = new(Environment.CurrentDirectory + @"\Logo.txt"); Console.WriteLine(sr.ReadToEnd()); } catch { }
             if (args.Length > 0) { _ = ulong.TryParse(args[0], out MonitorBot.GuildId); Logger.Info($"GuildId is designated. GuildId: {MonitorBot.GuildId}"); }
             return MonitorBot.Instance.MainAsync();
